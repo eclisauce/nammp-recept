9c250e774071468763b6f399edd2b14cd8e9d328
Unknown <lecke.martin@gmail.com>
2018-09-11 10:34:48 +0200

    Serve index.html on all urls

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-// Require the express module
 const express = require('express');
-// Create a new web server
 const app = express();
+const path = require('path');
+const publicPath = path.join(__dirname, 'www');
+
 // Tell the web server to serve files
 // from the www folder
 app.use(express.static('www'));
-// Start the web server on port 3000
-app.listen(3000,() => console.log('Listening on port 3000'));
+
+// Serves index.html on all urls
+app.use('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.listen(3000, () => console.log('Listening on port 3000'));
