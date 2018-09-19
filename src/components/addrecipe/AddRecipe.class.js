d1fc0fb812f0b85ea6f8f255e6b74dbffccead5a
Niklas J <Niklas.Jansson91@hotmail.com>
2018-09-19 13:36:53 +0200

    Extracts data of form and saves to obj

--- src/components/addrecipe/AddRecipe.class.js
+++ src/components/addrecipe/AddRecipe.class.js
@@ -14,7 +14,7 @@ export default class AddRecipe extends Base {
   constructor() {
     super();
     this.eventHandler();
-    this.formCounter = 0;
+    this.ingredientCounter = 0;
     this.checkIfFormsAreFilled()
   }
 
@@ -26,12 +26,16 @@ export default class AddRecipe extends Base {
   eventHandler() {
     $(document).on('click', '.delete-button', function (e) {
       e.preventDefault();
-      let formNumber = `form-${$(this).data('deleteButtonId')}`;
-      $(`#${formNumber}`).remove();
+      let ingredientInput = `ingredientInput-${$(this).data('deleteButtonId')}`;
+      $(`#${ingredientInput}`).remove();
     })
 
     $(document).on('change', '#number-of-portions', () => {
       this.getSelectedPortions();
+    });
+    let that = this;
+    $(document).on('submit', 'form', function(e) {
+      that.submitForm(e, this)
     });
   }
 
@@ -76,7 +80,45 @@ export default class AddRecipe extends Base {
 
   renderNewForm() {
     this.render('.add-ingredients-holder', 'template2')
-    this.formCounter++;
+    this.ingredientCounter++;
+  }
+
+  submitForm(e, formHtml) {
+    e.preventDefault();
+    let allFormData = $(formHtml).serializeArray();
+    let ingredientsPerPortion = [];
+
+    for (let i = 0; i < allFormData.length; i++) {
+      if (allFormData[i].name == 'Ingrediens') {
+        let ingredient = {};
+        ingredient.name = allFormData[i].value;
+        ingredient.quantity = allFormData[i+1].value;
+        ingredient.unitOfMeasurement = allFormData[i+2].value;
+        ingredient.lmTitle = allFormData[i+3].value;
+        ingredient.grams = allFormData[i+4].value;
+
+        ingredientsPerPortion.push(ingredient);
+      }
+      
+    }
+    
+    let modifiedRecipe = allFormData.reduce((obj, current)=>{
+      obj[current.name] = current.value;
+      return obj;
+    }, {});
+
+    delete modifiedRecipe.Ingrediens;
+    delete modifiedRecipe.Antal;
+    delete modifiedRecipe.Enhetsmått;
+    delete modifiedRecipe.IngrediensPerGram;
+    delete modifiedRecipe.IngrediensnamnLivsmedelsverket;
+
+    modifiedRecipe.ingredientsPerPortion = ingredientsPerPortion;
+
+    console.log(modifiedRecipe, "modified recipe obj")
+
+
+    // fs.writeFile('./www/json/aa.json', req.body); // backend-code
   }
 
 }
