08634ed2a5b39c1b174bcc10e6f795fb13eafde2
Unknown <petter.f.andersson@gmail.com>
2018-09-20 15:30:46 +0200

    Removed extra header element in header, visual changes for consistency

--- src/components/navbar/navbar.template.js
+++ src/components/navbar/navbar.template.js
@@ -1,35 +1,32 @@
 export default `
-  <header class="">
-    <nav class="navbar navbar-expand-lg navbar-light p-3 px-md-0 py-md-5 container">
-      <a class="navbar-brand pop" href="/"><span>Recept</span><span>&</span><span>Näring</span></a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbarNav"
-        aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <!-- Collapsing Navigation -->
-      <div class="collapse navbar-collapse" id="navbar">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item px-4">
-            <a class="nav-link pop" href="/">Hem</a>
-          </li>
-          <li class="nav-item px-4">
-            <a class="nav-link" href="/searchresult">Recept</a>
-          </li>
-          <li class="nav-item px-4">
-            <a class="nav-link pop" href="/add-recipe">Lägg till recept</a>
-          </li>
-        </ul>
-      </div>
-      <!-- // Collapsing Navigation End -->
-    </nav>
-    <!-- Search field -->
-
-    <div class="border search-holder ">
-      <div class="container header__search ">
-        <i class="fas fa-search"></i>
-        <input class="form-control form-control-lg pl-5 border-0" type="text" placeholder="Sök på recept..."></input>
-      </div>
+  <nav class="navbar navbar-expand-lg navbar-light p-3 px-md-0 py-md-5 container">
+    <a class="navbar-brand pop" href="/"><span>Recept</span><span>&</span><span>Näring</span></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbarNav"
+      aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <!-- Collapsing Navigation -->
+    <div class="collapse navbar-collapse" id="navbar">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item px-4 mt-md-3 mt-lg-0">
+          <a class="nav-link pop" href="/">Hem</a>
+        </li>
+        <li class="nav-item px-4">
+          <a class="nav-link" href="/searchresult">Recept</a>
+        </li>
+        <li class="nav-item px-4">
+          <a class="nav-link pop" href="/add-recipe">Lägg till recept</a>
+        </li>
+      </ul>
     </div>
-  </header>
-  <!-- // HEADER END -->
+    <!-- // Collapsing Navigation End -->
+  </nav>
+
+  <!-- Search field -->
+  <div class="border search-holder ">
+    <div class="container header__search ">
+      <i class="fas fa-search"></i>
+      <input class="form-control form-control-lg pl-5 border-0" type="text" placeholder="Sök på recept..."></input>
+    </div>
+  </div>
 `;
