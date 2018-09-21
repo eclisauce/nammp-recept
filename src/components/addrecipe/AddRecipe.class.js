63a04d7c2ecee534dfc194ec9eac6b331946459d
Markus Andersson <make_99@hotmail.com>
2018-09-21 09:31:43 +0200

    Started adding my code from broken branch

--- src/components/addrecipe/AddRecipe.class.js
+++ src/components/addrecipe/AddRecipe.class.js
@@ -1,10 +1,5 @@
 import Base from '../../base/Base.class';
-import {
-  template,
-  ingredientTemplate,
-  pictureUploadTemplate,
-  instructionTemplate
-} from './AddRecipe.template';
+import {template, ingredientTemplate, pictureUploadTemplate, instructionTemplate} from './AddRecipe.template';
 
 /**
  * Startpage in main for route '/'
@@ -15,8 +10,60 @@ export default class AddRecipe extends Base {
   constructor() {
     super();
     this.eventHandler();
+    this.loadJSONFood();
+    this.str;
     this.ingredientCounter = 0;
     this.instructionCounter = 1;
+  }
+
+  /**
+   * autocomplete method and sorting
+   * needs more added to it?
+   */
+  autoComplete(str) {
+    //   if(str.length < 3){
+    //   return new Error('You must send a str with >= 3 letters');
+    // }
+    str = str.toLowerCase();
+    return this.foodData.filter(x => x.Namn.toLowerCase().includes(str)).map(x => x.Namn).sort((a, b) => {
+
+      // advanced
+      // prioritize when finding the str
+      // as a separate word
+      let aIsSeparateWord = (' ' + a + ' ').toLowerCase().includes(' ' + str + ' ');
+      let bIsSeparateWord = (' ' + b + ' ').toLowerCase().includes(' ' + str + ' ');
+
+      // prioritize str early in name
+      let aPos = a.toLowerCase().indexOf(str) - (
+        aIsSeparateWord
+        ? 1000
+        : 0);
+      let bPos = b.toLowerCase().indexOf(str) - (
+        bIsSeparateWord
+        ? 1000
+        : 0);
+
+      if (aPos === bPos) {
+        // if same position
+        // sort alphabetically by name
+        return a < b
+          ? -1
+          : 1;
+      }
+
+      return aPos < bPos
+        ? -1
+        : 1;
+    });
+  }
+
+  /**
+   * async function to load in the ingredients json
+   *
+   * @author Markus
+   */
+  async loadJSONFood() {
+    this.foodData = await $.getJSON("/json/livsmedel.json");
   }
 
   change() {
@@ -39,15 +86,12 @@ export default class AddRecipe extends Base {
   }
 
   keydown() {
-      /**
+    /**
       * Keyup on tab for adding more instruction fields
       * @author Martin
       */
-    if ($(event.target).hasClass('instruction')
-        && $(event.target).val() !== ''
-        && (event.keyCode == 9 || event.which == 9)
-      ) {
-     this.renderNewInstruction();
+    if ($(event.target).hasClass('instruction') && $(event.target).val() !== '' && (event.keyCode == 9 || event.which == 9)) {
+      this.renderNewInstruction();
     }
 
   }
@@ -65,26 +109,46 @@ export default class AddRecipe extends Base {
     });
 
     /**
+ * jQuery code for handeling the input from user displaying the list from json and autocompletes
+ *
+ */
+    $(document).on('keyup', '.ingredient-input', function() {
+      let str = $(this).val();
+      let ul = $(this).parent().find('.result-dropdown');
+      let inputField = $(this);
+      ul.empty();
+      let foodItems = that.autoComplete(str);
+
+      for (let foodItem of foodItems) {
+        ul.append(`<li class="list-group-item list-item">${foodItem}</li>`);
+      }
+
+      $('.result-dropdown').on('click', '.list-item', function() {
+        let click_text = $(this).text().split('|');
+        $(inputField).val($.trim(click_text[0]));
+        $(".result-dropdown").html('');
+      });
+
+    });
+
+    /**
     * Handle checkboxes focus on tab will 'hover' the styled checkbox
     @author Martin
     */
     $(document).on('focus', 'input[type="checkbox"]', function(e) {
-          $(event.target).siblings('span').addClass('border-checkbox-hover');
-      }
-    );
+      $(event.target).siblings('span').addClass('border-checkbox-hover');
+    });
     $(document).on('blur', 'input[type="checkbox"]', function(e) {
-          $(event.target).siblings('span').removeClass('border-checkbox-hover');
-      }
-    );
+      $(event.target).siblings('span').removeClass('border-checkbox-hover');
+    });
   }
-
 
   click() {
     /**
     * Click on #add-instr to add more instructionfields
     * @author Martin
     */
-    if($(event.target).attr('id') === 'add-instr') {
+    if ($(event.target).attr('id') === 'add-instr') {
       this.renderNewInstruction();
     }
 
@@ -92,8 +156,8 @@ export default class AddRecipe extends Base {
     * Click on #remove-instr to delete current instructionfield
     * @author Martin
     */
-    if($(event.target).hasClass('remove-instr') || $(event.target).parent().hasClass('remove-instr')) {
-      if(event.toElement.nodeName === 'I') {
+    if ($(event.target).hasClass('remove-instr') || $(event.target).parent().hasClass('remove-instr')) {
+      if (event.toElement.nodeName === 'I') {
         $(event.target).parent().parent().fadeOut('slow', function() {
           $(this).remove();
         });
@@ -112,13 +176,12 @@ export default class AddRecipe extends Base {
       this.checkIfFormsAreFilled();
     }
 
-
     /**
     * Delete button - Deletes ingredient row when clicked
     * @author Martin
     */
-    if($(event.target).hasClass('delete-button') || $(event.target).parent().hasClass('delete-button')) {
-      if(event.toElement.nodeName === 'I') {
+    if ($(event.target).hasClass('delete-button') || $(event.target).parent().hasClass('delete-button')) {
+      if (event.toElement.nodeName === 'I') {
         $(event.target).parent().parent().parent().fadeOut('slow', function() {
           $(this).remove();
         });
@@ -133,9 +196,10 @@ export default class AddRecipe extends Base {
     * Button for adding a new line of ingredient
     * @author Martin
     */
-    $(event.target).attr('id') === 'add-form' ? this.renderNewForm() : null ;
+    $(event.target).attr('id') === 'add-form'
+      ? this.renderNewForm()
+      : null;
   }
-
 
   /**
   * Rendering number of portions selected to be displayed on h5.
@@ -163,7 +227,7 @@ export default class AddRecipe extends Base {
   */
   renderNewInstruction() {
     $('.instr-warning').remove();
-    if($(document).find('.instruction').last().val() !== '') {
+    if ($(document).find('.instruction').last().val() !== '') {
       this.instructionCounter++
       this.render('.instruction-container', 'instructionTemplate');
     } else {
@@ -181,17 +245,17 @@ export default class AddRecipe extends Base {
       if (allFormData[i].name == 'Ingrediens') {
         let ingredient = {};
         ingredient.name = allFormData[i].value;
-        ingredient.quantity = allFormData[i+1].value;
-        ingredient.unitOfMeasurement = allFormData[i+2].value;
-        ingredient.lmTitle = allFormData[i+3].value;
-        ingredient.grams = allFormData[i+4].value;
+        ingredient.quantity = allFormData[i + 1].value;
+        ingredient.unitOfMeasurement = allFormData[i + 2].value;
+        ingredient.lmTitle = allFormData[i + 3].value;
+        ingredient.grams = allFormData[i + 4].value;
 
         ingredientsPerPortion.push(ingredient);
       }
 
     }
 
-    let modifiedRecipe = allFormData.reduce((obj, current)=>{
+    let modifiedRecipe = allFormData.reduce((obj, current) => {
       obj[current.name] = current.value;
       return obj;
     }, {});
@@ -206,8 +270,7 @@ export default class AddRecipe extends Base {
 
     console.log(modifiedRecipe, "modified recipe obj")
 
-
-    // fs.writeFile('./www/json/aa.json', req.body); // backend-code
+    // fs.writeFile('./www/json/aa.json', req.body);  backend-code
   }
 
 }
