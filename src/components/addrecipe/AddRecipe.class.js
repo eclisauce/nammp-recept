04714b30af98788492d7e012a0aecfb2623d99cf
Unknown <andreas@stickydirt.se>
2018-09-21 19:47:05 +0200

    Add all checks working except URL-start.

--- src/components/addrecipe/AddRecipe.class.js
+++ src/components/addrecipe/AddRecipe.class.js
@@ -5,7 +5,9 @@ import {
   pictureUploadTemplate,
   instructionTemplate
 } from './AddRecipe.template';
-import { doesNotThrow } from 'assert';
+import {
+  doesNotThrow
+} from 'assert';
 
 /**
  * Startpage in main for route '/'
@@ -78,41 +80,41 @@ export default class AddRecipe extends Base {
   }
 
 
-  keyup(){
-    if ($(event.target).is('input, textarea')){
+  keyup() {
+    if ($(event.target).is('input, textarea')) {
       $(event.target).removeClass('red-border');
     }
   }
 
   click() {
     /**
-    * Click on #add-instr to add more instructionfields
-    * @author Martin
-    */
-    if($(event.target).attr('id') === 'add-instr') {
+     * Click on #add-instr to add more instructionfields
+     * @author Martin
+     */
+    if ($(event.target).attr('id') === 'add-instr') {
       this.renderNewInstruction();
     }
 
     /**
-    * Click on #remove-instr to delete current instructionfield
-    * @author Martin
-    */
-    if($(event.target).hasClass('remove-instr') || $(event.target).parent().hasClass('remove-instr')) {
-      if(event.toElement.nodeName === 'I') {
-        $(event.target).parent().parent().fadeOut('slow', function() {
+     * Click on #remove-instr to delete current instructionfield
+     * @author Martin
+     */
+    if ($(event.target).hasClass('remove-instr') || $(event.target).parent().hasClass('remove-instr')) {
+      if (event.toElement.nodeName === 'I') {
+        $(event.target).parent().parent().fadeOut('slow', function () {
           $(this).remove();
         });
       } else {
-        $(event.target).parent().fadeOut('slow', function() {
+        $(event.target).parent().fadeOut('slow', function () {
           $(this).remove();
         });
       }
     }
 
     /**
-    * Test purpop
-    * @author Andy / Niklas
-    */
+     * Test purpop
+     * @author Andy / Niklas
+     */
     if ($(event.target).attr('id') == 'test') {
       this.checkIfFormsAreFilled();
     }
@@ -143,17 +145,17 @@ export default class AddRecipe extends Base {
 
 
   /**
-  * Rendering number of portions selected to be displayed on h5.
-  * @author Andy
-  */
+   * Rendering number of portions selected to be displayed on h5.
+   * @author Andy
+   */
   getSelectedPortions() {
     $('.display-portions').empty('').append(`Ingredienser för ${$('#number-of-portions').val()} portioner`);
   }
 
   /**
-  * Rendering new form for Ingredient. Also autofocus the new Ingredientfield rendered
-  * @author Andy / Martin
-  */
+   * Rendering new form for Ingredient. Also autofocus the new Ingredientfield rendered
+   * @author Andy / Martin
+   */
   renderNewForm() {
     this.render('.add-ingredients-holder__list', 'ingredientTemplate')
     this.ingredientCounter++;
@@ -163,12 +165,12 @@ export default class AddRecipe extends Base {
   }
 
   /**
-  * Rendering new formfield for Instruction. Warns if last field is empty
-  *
-  */
+   * Rendering new formfield for Instruction. Warns if last field is empty
+   *
+   */
   renderNewInstruction() {
     $('.instr-warning').remove();
-    if($(document).find('.instruction').last().val() !== '') {
+    if ($(document).find('.instruction').last().val() !== '') {
       this.instructionCounter++
       this.render('.instruction-container', 'instructionTemplate');
     } else {
@@ -182,156 +184,176 @@ export default class AddRecipe extends Base {
   submitForm(e, formHtml) {
     e.preventDefault();
     let allFormData = $(formHtml).serializeArray();
-    console.log(allFormData);
-    this.checkIfNumbers(allFormData);
+    if (this.doAllChecks(allFormData)) {
 
-    let ingredientsPerPortion = [];
+      let ingredientsPerPortion = [];
 
-    // Get number of portions
-    let numberOfPortions = allFormData.filter(data => data.name === 'portions')[0].value;
+      // Get number of portions
+      let numberOfPortions = allFormData.filter(data => data.name === 'portions')[0].value;
 
-    // Get checked filters into an array.
-    let filters = allFormData.filter(data => data.value === 'on').map(filter => filter.name);
+      // Get checked filters into an array.
+      let filters = allFormData.filter(data => data.value === 'on').map(filter => filter.name);
 
-    // Filter instructions, get the values in the instructions, remove undefined or empty string
-    let instructions = allFormData.filter(data => data.name === 'instruction').map(ins => ins.value).filter(ins => ins !== "" || undefined);
+      // Filter instructions, get the values in the instructions, remove undefined or empty string
+      let instructions = allFormData.filter(data => data.name === 'instruction').map(ins => ins.value).filter(ins => ins !== "" || undefined);
 
 
-    for (let i = 0; i < allFormData.length; i++) {
-      if (allFormData[i].name == 'Ingrediens') {
-        let ingredient = {};
-        ingredient.name = allFormData[i].value;
-        ingredient.quantity = (allFormData[i + 1].value * 1) / numberOfPortions;
-        ingredient.unitOfMeasurement = allFormData[i + 2].value;
-        ingredient.lmTitle = allFormData[i + 3].value;
-        ingredient.grams = (allFormData[i + 4].value * 1) / numberOfPortions;
-        ingredientsPerPortion.push(ingredient);
+      for (let i = 0; i < allFormData.length; i++) {
+        if (allFormData[i].name == 'Ingrediens') {
+          let ingredient = {};
+          ingredient.name = allFormData[i].value;
+          ingredient.quantity = (allFormData[i + 1].value * 1) / numberOfPortions;
+          ingredient.unitOfMeasurement = allFormData[i + 2].value;
+          ingredient.lmTitle = allFormData[i + 3].value;
+          ingredient.grams = (allFormData[i + 4].value * 1) / numberOfPortions;
+          ingredientsPerPortion.push(ingredient);
+        }
       }
+
+      let modifiedRecipe = allFormData.reduce((obj, current) => {
+        obj[current.name] = current.value;
+        return obj;
+      }, {});
+
+
+
+      delete modifiedRecipe.Ingrediens;
+      delete modifiedRecipe.Antal;
+      delete modifiedRecipe.Enhetsmått;
+      delete modifiedRecipe.IngrediensPerGram;
+      delete modifiedRecipe.IngrediensnamnLivsmedelsverket;
+      delete modifiedRecipe.instruction;
+      delete modifiedRecipe.portions;
+
+      modifiedRecipe.ingredientsPerPortion = ingredientsPerPortion;
+
+      // Set portions to 1
+      modifiedRecipe.portions = 1;
+
+      // Delete filter objects
+      filters.forEach(filter => {
+        delete modifiedRecipe[filter];
+      })
+
+      // Set instructions
+      modifiedRecipe.instructions = instructions;
+
+      // Set filters
+      modifiedRecipe.filters = filters;
+
+      // Set an imgAlt to the title.
+      modifiedRecipe.imgAlt = modifiedRecipe.title;
+
+      // Make time to number
+      modifiedRecipe.time = modifiedRecipe.time * 1;
+
+
+
+      // Get full nutrition mother fuckkcccaass
+      let allNutritionsArray = modifiedRecipe.ingredientsPerPortion.map(val => {
+        if (val.lmTitle) {
+          return this.getAllNutritions(val.lmTitle, val.grams)
+        };
+      }).filter(nut => nut !== undefined);
+
+      // This is a reducer function that adds everything in an array.
+      const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+      modifiedRecipe.nutrientsPerPortion = {};
+      modifiedRecipe.nutrientsPerPortion.calories = allNutritionsArray.map(val => val.calories).reduce(reducer)
+      modifiedRecipe.nutrientsPerPortion.carbohydrates = allNutritionsArray.map(val => val.carbohydrates).reduce(reducer);
+      modifiedRecipe.nutrientsPerPortion.protein = allNutritionsArray.map(val => val.protein).reduce(reducer);
+      modifiedRecipe.nutrientsPerPortion.salt = allNutritionsArray.map(val => val.salt).reduce(reducer);
+      modifiedRecipe.nutrientsPerPortion.fat = {};
+      modifiedRecipe.nutrientsPerPortion.fat.monounsaturated = allNutritionsArray.map(val => val.fat.monounsaturated).reduce(reducer);
+      modifiedRecipe.nutrientsPerPortion.fat.saturated = allNutritionsArray.map(val => val.fat.saturated).reduce(reducer);
+      modifiedRecipe.nutrientsPerPortion.fat.polyunsaturated = allNutritionsArray.map(val => val.fat.polyunsaturated).reduce(reducer);
+      modifiedRecipe.nutrientsPerPortion.fat.total = allNutritionsArray.map(val => val.fat.total).reduce(reducer);
+
+
+      JSON._save('test-recipes.json', modifiedRecipe).then(function () {
+        console.log('Saved!');
+      });
+      console.log(modifiedRecipe, "modified recipe obj")
+      // This line of code gets all the ingredients.
+      // fs.writeFile('./www/json/aa.json', req.body); // backend-code
     }
-
-    let modifiedRecipe = allFormData.reduce((obj, current) => {
-      obj[current.name] = current.value;
-      return obj;
-    }, {});
-
-
-
-    delete modifiedRecipe.Ingrediens;
-    delete modifiedRecipe.Antal;
-    delete modifiedRecipe.Enhetsmått;
-    delete modifiedRecipe.IngrediensPerGram;
-    delete modifiedRecipe.IngrediensnamnLivsmedelsverket;
-    delete modifiedRecipe.instruction;
-    delete modifiedRecipe.portions;
-
-    modifiedRecipe.ingredientsPerPortion = ingredientsPerPortion;
-
-    // Set portions to 1
-    modifiedRecipe.portions = 1;
-
-    // Delete filter objects
-    filters.forEach(filter => {
-      delete modifiedRecipe[filter];
-    })
-
-    // Set instructions
-    modifiedRecipe.instructions = instructions;
-
-    // Set filters
-    modifiedRecipe.filters = filters;
-
-    // Set an imgAlt to the title.
-    modifiedRecipe.imgAlt = modifiedRecipe.title;
-
-    // Make time to number
-    modifiedRecipe.time = modifiedRecipe.time * 1;
-
-
-
-    // Get full nutrition mother fuckkcccaass
-    let allNutritionsArray = modifiedRecipe.ingredientsPerPortion.map(val => {
-      if(val.lmTitle) {return this.getAllNutritions(val.lmTitle, val.grams)};
-    }).filter(nut => nut !== undefined);
-
-    // This is a reducer function that adds everything in an array.
-    const reducer = (accumulator, currentValue) => accumulator + currentValue;
-
-    modifiedRecipe.nutrientsPerPortion = {};
-    modifiedRecipe.nutrientsPerPortion.calories = allNutritionsArray.map(val => val.calories).reduce(reducer)
-    modifiedRecipe.nutrientsPerPortion.carbohydrates = allNutritionsArray.map(val => val.carbohydrates).reduce(reducer);
-    modifiedRecipe.nutrientsPerPortion.protein = allNutritionsArray.map(val => val.protein).reduce(reducer);
-    modifiedRecipe.nutrientsPerPortion.salt = allNutritionsArray.map(val => val.salt).reduce(reducer);
-    modifiedRecipe.nutrientsPerPortion.fat = {};
-    modifiedRecipe.nutrientsPerPortion.fat.monounsaturated = allNutritionsArray.map(val => val.fat.monounsaturated).reduce(reducer);
-    modifiedRecipe.nutrientsPerPortion.fat.saturated = allNutritionsArray.map(val => val.fat.saturated).reduce(reducer);
-    modifiedRecipe.nutrientsPerPortion.fat.polyunsaturated = allNutritionsArray.map(val => val.fat.polyunsaturated).reduce(reducer);
-    modifiedRecipe.nutrientsPerPortion.fat.total = allNutritionsArray.map(val => val.fat.total).reduce(reducer);
-
-
-    JSON._save('test-recipes.json', modifiedRecipe).then(function(){
-      console.log('Saved!');
-    });
-    console.log(modifiedRecipe, "modified recipe obj")
-    // This line of code gets all the ingredients.
-    // fs.writeFile('./www/json/aa.json', req.body); // backend-code
   }
 
-  
+
+
+
   /**
-  * Receives all form data and checks if everything is filled out.
-  * If everything is filled out it calls the next method.
-  * Else it writes a error msg and makes a red border where the error is.
-  * @author Andy
-  */
-  checkIfEverythingHasValue(allFormData){
+   * Creates recipe Url from String
+   * @author Martin
+   */
+  createRecipeUrl(str) {
+    str = str.toLowerCase()
+      .replace(/[^a-zA-Z ]/g, '')
+      .replace(/å|ä/g, 'a')
+      .replace(/ö/g, 'o')
+      .replace(/\s/g, '');
+    return `${str.slice(0,15)}-${Date.now()}`;
+  }
+
+  doAllChecks(allFormData){
+    if(
+      this.checkIfEverythingHasValue(allFormData)
+      &&
+      this.checkIfExistsInDatabase(allFormData)
+      &&
+      this.checkIfNumbers(allFormData)
+      && 
+      this.checkIfFilterSelected(allFormData)
+    ) {
+      return true;
+    }
+  }
+
+
+  /**
+   * Receives all form data and checks if everything is filled out.
+   * If everything is filled out it calls the next method.
+   * Else it writes a error msg and makes a red border where the error is.
+   * @author Andy
+   */
+  checkIfEverythingHasValue(allFormData) {
     let emptyValueArray = allFormData.filter(form => form.value === "");
     console.log(emptyValueArray);
-    if (emptyValueArray.length === 0){
+    if (emptyValueArray.length === 0) {
       $('.something-went-wrong').empty();
-      // Call exists in database
+      return true;
     } else {
-      $('form input, textarea').each(function(){
-        if($(this).val() == ""){
+      $('form input, textarea').each(function () {
+        if ($(this).val() == "") {
           $(this).addClass('red-border');
 
           $('.something-went-wrong').empty().append('Alla fält måste vara ifyllda!');
         }
       })
+      return false;
     }
   }
 
   /**
-  * Creates recipe Url from String
-  * @author Martin
-  */
-  createRecipeUrl(str) {
-    str = str.toLowerCase()
-             .replace(/[^a-zA-Z ]/g, '')
-             .replace(/å|ä/g, 'a')
-             .replace(/ö/g, 'o')
-             .replace(/\s/g, '');
-    return `${str.slice(0,15)}-${Date.now()}`;
-  }
-
-  /**
-  * Receive a string and checks if that string exist in names of database.
-  * Returns true if it's found and false if it isn't found.
-  * @author Andy
-  */
-  existInDatabase(str){
+   * Receive a string and checks if that string exist in names of database.
+   * Returns true if it's found and false if it isn't found.
+   * @author Andy
+   */
+  existInDatabase(str) {
     return this.foodData.some(x => x.Namn == str);
   }
 
   /**
-  * Receives all form data and checks if everything is in the database.
-  * If everything is in the database it calls next function
-  * Else it writes a error msg and makes a red border where the error is.
-  * @author Andy
-  */
-  checkIfExistsInDatabase(allFormData){
+   * Receives all form data and checks if everything is in the database.
+   * If everything is in the database it calls next function
+   * Else it writes a error msg and makes a red border where the error is.
+   * @author Andy
+   */
+  checkIfExistsInDatabase(allFormData) {
     // Get all ingredients thats suppose to exist in database
     let ingredientsLm = allFormData.filter(data => {
-      if(data.name === "IngrediensnamnLivsmedelsverket") {
+      if (data.name === "IngrediensnamnLivsmedelsverket") {
         return true;
       }
     });
@@ -340,76 +362,91 @@ export default class AddRecipe extends Base {
     // Trying to loop out and compare ingredients lm to fooddata
     let tempArr = []
     ingredientsLm.forEach(x => {
-      if(!this.existInDatabase(x.value)){
+      if (!this.existInDatabase(x.value)) {
         tempArr.push(x);
-      } 
+      }
     })
 
     ingredientsLm = tempArr;
 
     // If all is found then ingredientsLm will be 0. 
-    if(ingredientsLm.length === 0){
-      // call numbercheck
+    if (ingredientsLm.length === 0) {
+      $('.something-went-wrong').empty();
+      return true;
     } else {
-
-
       let lmInputs = $('input[name="IngrediensnamnLivsmedelsverket"]');
-
       ingredientsLm.forEach(x => {
-        for (let i = 0; i < lmInputs.length; i++){
-          if(lmInputs.eq(i).val() == x.value) {
+        for (let i = 0; i < lmInputs.length; i++) {
+          if (lmInputs.eq(i).val() == x.value) {
             $(lmInputs.eq(i)).addClass('red-border');
 
             $('.something-went-wrong').empty().append('Alla ingredienser finns inte i databasen!');
           }
         }
       })
+      return false;
     }
   }
 
 
-
-
-  checkIfNumbers(allFormData){
+  /**
+   * Receives all form data and checks if some inputs are numbers.
+   * If they are then it calls the next function
+   * Else it writes a error msg and makes a red border where the error is.
+   * @author Andy
+   */
+  checkIfNumbers(allFormData) {
     let numbersOnlyArray = allFormData.filter(form => {
-      if(form.name == "time" || form.name == "Antal" || form.name == "IngrediensPerGram"){
+      if (form.name == "time" || form.name == "Antal" || form.name == "IngrediensPerGram") {
         return form;
       };
     });
 
     let isNumbers = numbersOnlyArray.map(numVal => numVal.value * 1);
     let isNotNumberArr = [];
-    console.log(isNumbers);
     isNumbers.forEach((x, index) => {
-      if(!(!isNaN(x) && x !== 0)){
+      if (!(!isNaN(x) && x !== 0)) {
         isNotNumberArr.push(numbersOnlyArray[index]);
       }
     })
 
-    if(isNotNumberArr.length === 0){
+    if (isNotNumberArr.length === 0) {
       // Call next function
       $('.something-went-wrong').empty();
+      return true;
     } else {
-      
+
       let numberInput = $('input[name="time"], input[name="Antal"], input[name="IngrediensPerGram"]');
 
       isNotNumberArr.forEach(x => {
-        for (let i = 0; i < numberInput.length; i++){
-          if(numberInput.eq(i).val() == x.value) {
+        for (let i = 0; i < numberInput.length; i++) {
+          if (numberInput.eq(i).val() == x.value) {
             $(numberInput.eq(i)).addClass('red-border');
 
             $('.something-went-wrong').empty().append('Var vänlig kontrollera att du matat in nummer på rätt ställe!');
           }
         }
       })
+      return false;
     }
-    console.log(isNotNumberArr);
+  }
+
+  checkIfFilterSelected(allFormData) {
+    let filters = allFormData.filter(data => data.value === 'on').map(filter => filter.name);
+    console.log(filters);
+    if (filters.length > 0) {
+      $('.something-went-wrong').empty();
+      return true;
+    } else {
+      $('.something-went-wrong').empty().append('Var snäll välj en kategori!');
+      return false;
+    }
   }
 
 
 
   /**
-   * Gets all the nutritions needed for the recipe and adds it to an object that is returned
+   * Receives all the nutritions needed for the recipe and adds it to an object that is returned
    * @author Andy
    */
   getAllNutritions(ingredientStr, grams) {
