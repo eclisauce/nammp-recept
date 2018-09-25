9843d7a92784d147bc2c9268d6be2556a2bd76fe
Unknown <andreas@stickydirt.se>
2018-09-25 09:11:57 +0200

    Add now getting all filters with space together.

--- src/base/Router.class.js
+++ src/base/Router.class.js
@@ -58,7 +58,7 @@ export default class Router {
     };
 
     function translateCharacters(str) {
-      return str.replace(/%C3%A5/g, "å").replace(/%C3%A4/g, "ä").replace(/%C3%B6/g, "ö").replace(/%C3%A9/g, "é").replace(/%20/g, " ");
+      return str.replace(/%C3%A5/g, "å").replace(/%C3%A4/g, "ä").replace(/%C3%B6/g, "ö").replace(/%C3%A9/g, "é");
     }
 
     /**
@@ -75,8 +75,7 @@ export default class Router {
       let filters = [];
       if (indexOfFilters > -1) {
         searchStr = url.substring(14, indexOfFilters);
-        filters = url.substring(indexOfFilters + 8).replace(/-/g, ' ').split(' ');
-        console.log(filters);
+        filters = url.substring(indexOfFilters + 8).replace(/-/g, ' ').split(' ').map(x => x.replace(/%20/g, ' '));
       }
       let methodName = urls[newUrl];
       // Call the right method
