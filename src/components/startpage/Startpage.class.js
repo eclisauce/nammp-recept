a88de79c3b6e36d8e58aec723eb72b8aaf7d488c
Niklas J <Niklas.Jansson91@hotmail.com>
2018-09-27 15:43:30 +0200

    Adds star styling and class to startpage.

--- src/components/startpage/Startpage.class.js
+++ src/components/startpage/Startpage.class.js
@@ -50,15 +50,15 @@ export default class Startpage extends Base {
           <div class="media-body">
             <h5 class="mt-0 media-heading d-inline-block">${recipe.title}<i class="fas fa-angle-right fa-lg"></i></h5>
             
-            <p class="w-100 mb-0 mt-2 mr-2 text-muted">Svårighetsgrad: 
-                <span>
-                  <i class="fas fa-star ${recipe.difficulty < 1 ? 'text-muted' : ''} mr-1"></i>
-                  <i class="fas fa-star ${recipe.difficulty < 2 ? 'text-muted' : ''} mr-1"></i>
-                  <i class="fas fa-star ${recipe.difficulty < 3 ? 'text-muted' : ''} mr-1"></i>
-                  <i class="fas fa-star ${recipe.difficulty < 4 ? 'text-muted' : ''} mr-1"></i>
-                  <i class="fas fa-star ${recipe.difficulty < 5 ? 'text-muted' : ''} mr-1"></i>
-                </span>
-              </p>
+            <p class="w-100 mb-0 mt-2 text-muted">Svårighetsgrad: 
+              <span class="stars px-2">
+                <i class="fas fa-star ${recipe.difficulty < 1 ? 'text-muted' : ''} mr-1"></i>
+                <i class="fas fa-star ${recipe.difficulty < 2 ? 'text-muted' : ''} mr-1"></i>
+                <i class="fas fa-star ${recipe.difficulty < 3 ? 'text-muted' : ''} mr-1"></i>
+                <i class="fas fa-star ${recipe.difficulty < 4 ? 'text-muted' : ''} mr-1"></i>
+                <i class="fas fa-star ${recipe.difficulty < 5 ? 'text-muted' : ''} mr-1"></i>
+              </span>
+            </p>
             <p class="my-1 my-sm-2">${recipe.description}</p>
             <div class="row recipe-info-wrapper">
               <p class="col-6 my-0 mt-sm-2 text-muted"><i class="fas fa-utensils mr-2"></i>${recipe.nutrientsPerPortion.calories.toFixed()} kalorier</p>
